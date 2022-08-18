c323d2bed80c41f1e6df99a11ad8180a3f8e4354
ramriddlez <ramankangz@outlook.com>
2022-08-18 14:12:58 -0400

    changes

--- myapp/src/components/Info.js
+++ myapp/src/components/Info.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Image from "./images/selfie.jpeg"
 
 export default function Info() {
     return (
         <>
-        
+        <img src= {Image} alt ="me"> </img>
         <h1> Raman Khangura </h1>
         <h4> Front end Developer </h4>
         <h6> laurasmith.website </h6>
